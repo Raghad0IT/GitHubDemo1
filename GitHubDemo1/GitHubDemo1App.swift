7025eca1c955e6cb39896c68b1e5b161f347a16b
Raghad0IT <raghadtuwaiq@gmail.com>
2024-09-26 17:01:40 +0300

    Initial Commit

---
+++ GitHubDemo1/GitHubDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemo1App.swift
+//  GitHubDemo1
+//
+//  Created by Raghad on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
